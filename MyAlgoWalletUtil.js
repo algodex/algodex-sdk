50069b0ff3c251e922f74a5e6f1345583db5fa20
Alexander Trefonas <atrefonas@gmail.com>
2021-06-25 19:41:06 -0400

    Change params to not use window

--- MyAlgoWalletUtil.js
+++ MyAlgoWalletUtil.js
@@ -1,4 +1,7 @@
 
+const constants = require('./constants.js');
+const axios = require('axios').default;
+
 exports.connectToWallet = async function(txn) {
     try {
         accounts = await myAlgoWallet.connect();
@@ -10,6 +13,28 @@ exports.connectToWallet = async function(txn) {
     }
 
 };
+
+const getParams = async() => {
+    try {
+        const response = await axios.get('https://testnet.algoexplorerapi.io/v2/transactions/params');
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw new Error("getAccountInfo failed: ", error);
+    }
+
+}
+
+
+
+let transactionParams = null;
+
+(async () => {
+    transactionParams = await getParams();
+})().catch(err => {
+    console.error(err);
+});
 
 
 exports.setTransactionFee = function(txn) {
