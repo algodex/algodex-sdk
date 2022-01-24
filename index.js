616cc5a608de996dc718050baf90de1d78f6d049
ericsharma <sharma.e@husky.neu.edu>
2022-01-24 16:21:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ exports.executeOrderAsTaker = function(algodClient, isSellingASA_AsTakerOrder, a
  * @param {Boolean} isSellingASA_AsTakerOrder: boolean true if the taker is selling the ASA to an ALGO-only escrow buy order
  * @param {Number}                    assetId: Algorand ASA ID for the asset.
  * @param {String}            takerWalletAddr: public address of the taker's wallet address
- * @param {Number}                 limitPrice: price of the base unit ASA in terms of microALGO
+ * @param {Number}                 currentMarketPrice: market price of the base unit ASA in terms of microALGO
  * @param {Number}                 worstAcceptablePrice: price of the base unit ASA in terms of microALGO after accounting for tolerance
  * @param {Number}                 tolerance: float from 0-1
  * @param {Number}           orderAssetAmount: Must be integer. max amount of the asset to buy or sell in base units
